2a6108b3f11c1a179a59a24a21cecb78aff2c3de
salambolog <salambolog@gmail.com>
2018-04-09 06:46:13 +0800

    Must do a git ignore

---
+++ js/script.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        animateOut: 'fadeOutRight',
+        animateIn: 'fadeInLeft',
+        autoplay: true,
+        touchDrag: true,
+        center: true,
+        margin: 20,
+        slideSpeed: 20000,
+        dotsEach: 3,
+        singleItem: true,
+        autoHeight: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                loop: true
+            },
+            768: {
+                items: 1,
+                nav: true,
+                loop: true
+            }
+        }
+    });
+});
