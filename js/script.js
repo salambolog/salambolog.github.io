026b90143107ea59108c30e49e555a265381c7a2
salambolog <salambolog@gmail.com>
2018-04-25 07:44:44 +0800

    Pushing new edition

--- js/script.js
+++ js/script.js
@@ -4,24 +4,23 @@
         items: 1,
         animateOut: 'fadeOutRight',
         animateIn: 'fadeInLeft',
-        autoplay: true,
+        // autoplay: true,
         touchDrag: true,
         center: true,
         margin: 20,
         slideSpeed: 20000,
-        dotsEach: 3,
+        nav: false,
+        // dotsEach: 3,
         singleItem: true,
         autoHeight: true,
         responsiveClass: true,
         responsive: {
             0: {
                 items: 1,
-                nav: true,
                 loop: true
             },
             768: {
                 items: 1,
-                nav: true,
                 loop: true
             }
         }
--- js/slide.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function($) {
-
-Galleria.loadTheme('js/galleria.classic.min.js');
-Galleria.run('.galleria');
-});
